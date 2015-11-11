09517adca53f194bb73a1ce8c50741cc92c1dcd8
rexzhu <rexzhu@rexzhudeiMac.local>
2015-11-11 10:02:58 +0800

    测试MPMusicPlayerController相关方法能否取到信息，结果：能取到系统播放器的播放信息，第三方app的不行

--- GetNowPlayingInfoExp/GetNowPlayingInfoExp/ViewController.m
+++ GetNowPlayingInfoExp/GetNowPlayingInfoExp/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+@import MediaPlayer;
+
 @interface ViewController ()
 
 @end
@@ -22,6 +24,38 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) currentPlayingInfo
+{
+//    MPMediaItem *item = [[MPMusicPlayerController iPodMusicPlayer] nowPlayingItem];//iPodMusicPlayer 8.0已经废弃，他能获取系统播放器的播放信息
+    
+    if ([MPMusicPlayerController respondsToSelector:@selector(systemMusicPlayer)]) {//7系统上怎么没这个方法
+        MPMediaItem *item = [[MPMusicPlayerController systemMusicPlayer] nowPlayingItem];
+        
+        NSLog(@"%@", [item valueForProperty:MPMediaItemPropertyTitle]);
+        NSLog(@">>>item=%@",item);
+    }
+    else
+    {
+        NSLog(@">>>不支持systemMusicPlayer。");
+    }
+    
+    
+    if ([MPMusicPlayerController respondsToSelector:@selector(applicationMusicPlayer)]) {
+        MPMediaItem *item = [[MPMusicPlayerController applicationMusicPlayer] nowPlayingItem];
+        
+        NSLog(@"%@", [item valueForProperty:MPMediaItemPropertyTitle]);
+        NSLog(@">>>applicationMusicPlayer item=%@",item);
+    }
+    else
+    {
+        NSLog(@">>>不支持applicationMusicPlayer。");
+    }
+    
+}
+- (IBAction)checkCurrentPlayingInfo:(id)sender {
+    [self currentPlayingInfo];
 }
 
 @end
